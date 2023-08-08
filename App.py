aaa5dfa580daffc6f804624ca0bb5fc80cf97014
Abdulkareem Sikirulahi <sikirulahi.abdulkareem@kibo.school>
2023-08-08 16:30:29 +0100

    Both video and audio working #6

--- App.py
+++ App.py
@@ -1,29 +1,27 @@
 from pytube import YouTube
 
+# Function for downloading the video
 def download_video(video_url, save_path, resolution_choice):
     try:
         youtube = YouTube(video_url)
-        video_stream = youtube.get_stream(resolution_choice)
+        video_stream = youtube.streams.get_by_resolution(resolution_choice)
         video_stream.download(output_path = save_path)
-        print("Downloading video...")
-        print("================================")
         print("Video downloaded successfully")
     except Exception as e:
         print("Failed to download video :" + str(e))
 
+# Function to download the audio file
 def download_audio(video_url, save_path):
     try:
         youtube = YouTube(video_url)
         audio = youtube.streams.filter(only_audio = True).first()
         audio.download(output_path =save_path)
-        print("Downloading audio")
-        print("================================")
         print("Downloaded Sussesfully")
     except Exception as e:
         print("Failed to download Audio :" + str(e))
     
     
-    
+# The main function
 def main():
     print("This is a Youtube downloader app cleated by Sikirulahi")
     print("Gmail: kareemsikiru2018@gmail.com")
@@ -36,11 +34,10 @@ def main():
     if choice == "1":
         resolution_choice = input("Choose a desired resolution(e.g., 720p, 1080p):")
         download_video(video_url, save_path, resolution_choice)
-    if choice == "2":
+    elif choice == "2":
         download_audio(video_url, save_path)
-    
     else:
-        print("Invalid Choice, Please Select a choice")
+        print("Invalid Choice, Please choose")
     
 if __name__ == "__main__":
     main()
